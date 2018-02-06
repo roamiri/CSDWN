721c730b65e1ee3063bc72c46988fff2509d0901
roamiri <amiriruhollah@gmail.com>
2018-02-06 11:28:03 -0700

    bugfix

--- parallel_R_pro.m
+++ parallel_R_pro.m
@@ -3,7 +3,7 @@
 %   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function parallel_R_pro(pref_poolSize)
-% parpool(pref_poolSize)
+parpool(pref_poolSize)
 BS_list = generate_network(40, 200, 200, 200,'mmWave',true);
 BS_list_size = size(BS_list,2);
 
@@ -13,11 +13,11 @@ for i=1:200
     permutationsMat(i,:) = randperm(BS_list_size,BS_list_size);
 end
 
-% parfor_progress(200);
- for i=1:200
+parfor_progress(200);
+ parfor i=1:200
     runForAll(BS_list,permutationsMat(i,:),i);
-%     pause(rand);
-%     parfor_progress;
+    pause(rand);
+    parfor_progress;
  end
-%  parfor_progress(0); % Clean up
+ parfor_progress(0); % Clean up
 end
